53899161536270d9bd956dbda41af710074d9b20
Sebastian Jara <sebajara91@gmail.com>
2016-08-08 16:32:34 -0300

    Binding fix.

--- app/src/main/java/com/toptier/targetmvd/ui/views/MainView.java
+++ app/src/main/java/com/toptier/targetmvd/ui/views/MainView.java
@@ -1,33 +1,23 @@
 package com.toptier.targetmvd.ui.views;
 
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 
 import com.squareup.otto.Bus;
 import com.toptier.targetmvd.R;
 import com.toptier.targetmvd.ui.activities.MainActivity;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
 public class MainView extends BaseView {
-    private MainActivity mActivity;
     private Bus mBus;
-
-    @Bind(R.id.sign_up)         Button signUpButton;
-    @Bind(R.id.sign_in)         Button signInButton;
-    @Bind(R.id.spinner_gender)  Spinner genderSpinner;
 
     public MainView(MainActivity activity, Bus bus) {
         super(activity);
         this.mBus = bus;
 
         ButterKnife.bind(this, activity);
-
-        initializeSpinner(activity);
     }
 
     @OnClick(R.id.sign_in)
@@ -38,15 +28,6 @@ public class MainView extends BaseView {
     @OnClick(R.id.sign_up)
     public void signUpOnClick() {
         mBus.post(new SignUpOnClickEvent());
-    }
-
-    private void initializeSpinner(MainActivity activity) {
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(activity,
-                R.array.genders_array, android.R.layout.simple_spinner_item);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        genderSpinner.setAdapter(adapter);
     }
 
     public static class SignUpOnClickEvent {}
--- app/src/main/java/com/toptier/targetmvd/ui/views/SignUpView.java
+++ app/src/main/java/com/toptier/targetmvd/ui/views/SignUpView.java
@@ -1,13 +1,35 @@
 package com.toptier.targetmvd.ui.views;
 
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import com.squareup.otto.Bus;
+import com.toptier.targetmvd.R;
 import com.toptier.targetmvd.ui.activities.SignUpActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SignUpView extends BaseView {
     private Bus mBus;
 
+    @Bind(R.id.spinner_gender) Spinner genderSpinner;
+
     public SignUpView(SignUpActivity activity, Bus bus) {
         super(activity);
         this.mBus = bus;
+
+        ButterKnife.bind(this, activity);
+
+        initializeSpinner();
+    }
+
+    private void initializeSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.genders_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        genderSpinner.setAdapter(adapter);
     }
 }
